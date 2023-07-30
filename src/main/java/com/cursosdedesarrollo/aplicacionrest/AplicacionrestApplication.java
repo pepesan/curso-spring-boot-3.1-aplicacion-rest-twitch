f4694f4c1de3e4a6e72ab9a31d47a852c8a50e9a
David Vaquero <pepesan@gmail.com>
2023-07-30 18:23:38 +0200

    primer commit

---
+++ src/main/java/com/cursosdedesarrollo/aplicacionrest/AplicacionrestApplication.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.aplicacionrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursosdedesarrollo/aplicacionrest/AplicacionrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.aplicacionrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
