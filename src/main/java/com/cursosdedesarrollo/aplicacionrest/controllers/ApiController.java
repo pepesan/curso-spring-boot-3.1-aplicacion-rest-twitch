6b3e6108f9ffa7fc89b72ab8e152893210b0d9a9
David Vaquero <pepesan@gmail.com>
2023-08-13 19:08:31 +0200

    Api Rest Completo * list * create * read * update * delete

--- src/main/java/com/cursosdedesarrollo/aplicacionrest/controllers/ApiController.java
+++ src/main/java/com/cursosdedesarrollo/aplicacionrest/controllers/ApiController.java
@@ -1,15 +1,15 @@
 package com.cursosdedesarrollo.aplicacionrest.controllers;
 
 import com.cursosdedesarrollo.aplicacionrest.dtos.Dato;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/dato")
+@Slf4j
 public class ApiController {
     public List<Dato> listado = new LinkedList<>();
     public Long lastID = 0L;
@@ -28,5 +28,56 @@ public class ApiController {
     @GetMapping("/")
     List<Dato> index(){
         return  this.listado;
+    }
+
+    /*
+        Post /api/v1/dato/ -> Crear un nuevo dato
+     */
+    @PostMapping("/")
+    Dato create(@RequestBody Dato dato){
+        dato.setId(++this.lastID);
+        this.listado.add(dato);
+        return dato;
+    }
+
+    /*
+        Get /api/v1/dato/{id} -> Obtener un dato
+     */
+    @GetMapping("/{id}")
+    Dato show(@PathVariable("id") Long id){
+        return this.listado
+                .stream()
+                .filter(dato -> dato.getId().equals(id))
+                .findFirst().orElse(new Dato());
+    }
+
+    /*
+        Patch /api/v1/dato/{id} -> Actualizar un dato
+     */
+    @PatchMapping("/{id}")
+    Dato update(@PathVariable("id") Long id, @RequestBody Dato dato){
+        return this.listado
+                .stream()
+                .filter(datoGuardado -> datoGuardado.getId().equals(id))
+                .findFirst()
+                .map(datoGuardado -> {
+                      datoGuardado.setCadena(dato.getCadena());
+                      return datoGuardado;
+                }).orElse(new Dato());
+    }
+    /*
+        Delete /api/v1/dato/{id} -> Eliminar un dato
+     */
+    @DeleteMapping("/{id}")
+    Dato delete(@PathVariable("id") Long id){
+        // busca el elemento en listado, lo elimina y lo devuelve
+        return this.listado
+               .stream()
+               .filter(datoGuardado -> datoGuardado.getId().equals(id))
+               .findFirst()
+               .map(datoGuardado -> {
+                      this.listado.remove(datoGuardado);
+                      return datoGuardado;
+                }).orElse(new Dato());
     }
 }
